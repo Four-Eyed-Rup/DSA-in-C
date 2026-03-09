c7ba3ba906e41ddcfec0281eb4a5d65408150c32
Swarup Das <dasswarup20182001@gmail.com>
2026-03-09 21:15:50 +0530

    Enhance selection sort with user input and output
    Updated selection sort implementation to allow user input and improved output formatting.

--- Sorting/selection.c
+++ Sorting/selection.c
@@ -1,43 +1,47 @@
-// Selection sort in C
+// Write a program for sorting an array by using selection sort 
 
 #include <stdio.h>
 
-// function to swap the the position of two elements
-void swap(int *a, int *b) {
-  int temp = *a;
-  *a = *b;
-  *b = temp;
-}
-
-void selectionSort(int array[], int size) {
-  for (int step = 0; step < size - 1; step++) {
-    int min_idx = step;
-    for (int i = step + 1; i < size; i++) {
-
-      // To sort in descending order, change > to < in this line.
-      // Select the minimum element in each loop.
-      if (array[i] < array[min_idx])
-        min_idx = i;
+void selection_sort(int arr[], int len)
+{
+  int i, j, min_idx, temp;
+  // selection sorting
+  for (i = 0; i < len-1; i++)
+  {
+    min_idx = i;
+    // smallest value initializing in min during traversing
+    for (j = i + 1; j < len ; j++)
+    {
+      if (arr[min_idx] > arr[j])
+      {
+        min_idx = j;
+      }
     }
-
-    // put min at the correct position
-    swap(&array[min_idx], &array[step]);
+    // swaping
+    temp = arr[min_idx];
+    arr[min_idx] = arr[i];
+    arr[i] = temp;
+     
+  }
+  printf("\n Sorted array------------------------------");
+  for(i=0; i<len; i++){
+    printf("\narr[%d]: %d", i, arr[i]);
   }
 }
 
-// function to print an array
-void printArray(int array[], int size) {
-  for (int i = 0; i < size; ++i) {
-    printf("%d  ", array[i]);
-  }
-  printf("\n");
-}
+int main()
+{
+  int i, n, arr[20];
+  printf("\n Enter the number of elements of array: ");
+  scanf("%d", &n);
 
-// driver code
-int main() {
-  int data[] = {20, 12, 10, 15, 2};
-  int size = sizeof(data) / sizeof(data[0]);
-  selectionSort(data, size);
-  printf("Sorted array in Acsending Order:\n");
-  printArray(data, size);
+  // initialize array elements
+  for (i = 0; i < n; i++)
+  {
+    printf("arr[%d]: ", i);
+    scanf("%d", &arr[i]);
+  }
+  selection_sort(arr, n);
+
+  return 0;
 }
