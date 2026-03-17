975f40c586c1f5e994a89952edda9954d7509306
Swarup Das <dasswarup20182001@gmail.com>
2026-03-17 23:26:39 +0530

    Add files via upload

---
+++ Linked-list/traversing_LL.c
@@ -0,0 +1,51 @@
+// Write a program to traverse a linked list and display all the elements
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+};
+struct node *start;
+
+struct node *create(struct node *start){
+    struct node *ptr, *newnode;
+    int num;
+    printf("\n Enter -1 for exit.");
+    printf("\n Enter the data: ");
+    scanf("%d", &num);
+    
+    while(num != -1){
+        newnode = (struct node*) malloc (sizeof(struct node));
+        newnode->data = num;
+        if(start == NULL){
+            start = newnode;
+            newnode->next = NULL;
+        }
+        else{
+            ptr = start;
+            while(ptr->next != NULL){
+                ptr = ptr->next;
+            }
+            ptr->next = newnode;
+            newnode->next = NULL;
+        }
+        printf("\n Enter the data2: ");
+        scanf("%d", &num);
+    }
+    return start;
+}
+
+struct node *display(struct node *start){
+    struct node *ptr = start;
+    printf("\n");
+    while(ptr != NULL){
+        printf("%d->", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL");
+}
+int main(){
+    start = create(start);
+    display(start);
+    return 0;
+}
