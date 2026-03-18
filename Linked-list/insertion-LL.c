188a85de68de65edb889cb9498c82289fe079af1
Swarup Das <dasswarup20182001@gmail.com>
2026-03-18 13:12:21 +0530

    Implement linked list creation, insertion, and display
    Added functionality for insertion and display of nodes in a linked list.

--- Linked-list/insertion-LL.c
+++ Linked-list/insertion-LL.c
@@ -1,3 +1,5 @@
+// white a program to implemnet creation, insertion and display of nodes in linked list
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -34,28 +36,63 @@ struct node *create(struct node *start)
             ptr->next = newnode;
             newnode->next = NULL;
         }
-    printf("\n Enter the data: ");
-    scanf("%d", &num);
+        printf("\n Enter the data 2 : ");
+        scanf("%d", &num);
     }
-    return start;
-};
-struct node *insert(struct node *start) {
-
-};
-struct node *display(struct node *start)
+return start;
+}
+struct node *insert(struct node *start)
 {
-    struct node *ptr;
-    ptr = start;
-    while (ptr->next != NULL)
-    {
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    int val;
+        printf("\n Enter the elemeent that has to be inserted: ");
+        scanf("%d", &val);
+        newnode->data = val;
+        newnode->next = start;
+        start = newnode;
+    return start;
+}
+
+struct node *display(struct node *start){
+    struct node *ptr = start;
+    printf("\n");
+    while(ptr != NULL){
         printf("%d->", ptr->data);
         ptr = ptr->next;
     }
     printf("NULL");
-};
+    return start;
+}
 int main()
 {
-    start = create(start);
-    display(start);
+    int choice;
+    printf("\n 1) New node  2) Insert 3) Display 4) Exit\n");
+    printf("\n Enter the choice: ");
+    scanf("%d", &choice);
+
+    while (choice != 4)
+    {
+        switch (choice)
+        {
+            case 1:
+                start = create(start);
+                break;
+            case 2:
+                start = insert(start);
+                break;
+            case 3:
+                start = display(start);
+                break;
+            case 4:
+                break;
+            default:
+            {
+                
+            }
+        }
+    printf("\n 1) New node  2) Insert 3) Display 4) Exit\n");
+    printf("\n Enter the choice: ");
+    scanf("%d", &choice);
+    }
     return 0;
 }
