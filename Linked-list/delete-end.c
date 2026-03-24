e7226eeff77c4713f5df5c59a86757ae59779ee7
Swarup Das <dasswarup20182001@gmail.com>
2026-03-24 15:24:42 +0530

    adding linked -list program for deletion
    delete a node from the end

---
+++ Linked-list/delete-end.c
@@ -0,0 +1,103 @@
+// Write a program to delete the firat node in linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *start;
+
+struct node *create(struct node *start)
+{
+    struct node *newnode, *ptr;
+    int val;
+    printf("\n Enter the data: ");
+    scanf("%d", &val);
+    while (val != -1)
+    {
+        newnode = (struct node *)malloc(sizeof(struct node));
+        newnode->data = val;
+        if (start == NULL)
+        {
+            newnode->next = NULL;
+            start = newnode;
+        }
+        else
+        {
+            ptr = start;
+            while (ptr->next != NULL)
+            {
+                ptr = ptr->next;
+            }
+            newnode->next = NULL;
+            ptr->next = newnode;
+        }
+        printf("\n Enter the data: ");
+        scanf("%d", &val);
+    }
+    return start;
+}
+struct node *delete_beginning(struct node *start)
+{
+    struct node *ptr, *preptr;
+    ptr = start;
+    if (start->next == NULL)
+    {
+        free(ptr);
+        start = NULL;
+    }
+    else
+    {
+        while (ptr->next != NULL)
+        {
+            preptr = ptr;
+            ptr = ptr->next;
+        }
+        preptr->next = NULL;
+        free(ptr);
+    }
+    return start;
+}
+struct node *display(struct node *start)
+{
+    struct node *ptr;
+    ptr = start;
+    while (ptr != NULL)
+    {
+        printf("%d-> ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL");
+    return start;
+}
+int main()
+{
+    int choice;
+    printf("\n 1) Create 2) Delete from end 3) Display 4) Exit");
+    printf("\n Enter the choice: ");
+    scanf("%d", &choice);
+    while (choice != 4)
+    {
+        switch (choice)
+        {
+        case 1:
+            start = create(start);
+            break;
+        case 2:
+            start = delete_beginning(start);
+            break;
+        case 3:
+            start = display(start);
+            break;
+        case 4:
+            break;
+        }
+        printf("\n 1)Create 2) Delete from end 3) Display 4) Exit");
+        printf("\n Enter the choice: ");
+        scanf("%d", &choice);
+    }
+    return 0;
+}
